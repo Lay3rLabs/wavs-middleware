73fb05b5015d51852f7dd7af3c24a2bd26becc99
Ron Turetzky <turetzkyron@gmail.com>
2025-03-06 16:53:19 -0500

    fix: implementing avsregistrar compat for v1.1.1

--- contracts/src/LayerAVSRegistrar.sol
+++ contracts/src/LayerAVSRegistrar.sol
@@ -6,7 +6,29 @@ import {IAVSRegistrar} from "@eigenlayer/contracts/interfaces/IAVSRegistrar.sol"
 // TODO: decide on AVSRegistar logic
 // Dummy AVSRegistrar contract for now
 contract LayerAVSRegistrar is IAVSRegistrar {
-    function registerOperator(address operator, uint32[] calldata operatorSetIds, bytes calldata data) external {}
-    function deregisterOperator(address operator, uint32[] calldata operatorSetIds) external {}
+    function registerOperator(
+        address operator,
+        address avs,
+        uint32[] calldata operatorSetIds,
+        bytes calldata data
+    ) external override {
+        // TODO: Implement registration logic
+    }
+
+    function deregisterOperator(
+        address operator,
+        address avs,
+        uint32[] calldata operatorSetIds
+    ) external override {
+        // TODO: Implement deregistration logic
+    }
+
+    function supportsAVS(
+        address avs
+    ) external view override returns (bool) {
+        // TODO: Implement logic to check if AVS is supported
+        return true; // Placeholder return value
+    }
+
     fallback () external {}
 }
