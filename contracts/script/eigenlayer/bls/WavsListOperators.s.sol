e3b88348f723622ade7430a6370d4c9cd585afc1
Supeeerpower <126687680+Supeeerpower@users.noreply.github.com>
2025-08-07 13:41:36 +0200

    script: bls list operator data struct

--- contracts/script/eigenlayer/bls/WavsListOperators.s.sol
+++ contracts/script/eigenlayer/bls/WavsListOperators.s.sol
@@ -2,18 +2,8 @@
 pragma solidity ^0.8.27;
 
 import {Script} from "forge-std/Script.sol";
-import {console} from "forge-std/console.sol";
-import {ISlashingRegistryCoordinator} from
-    "@eigenlayer-middleware/src/interfaces/ISlashingRegistryCoordinator.sol";
-import {IStakeRegistry} from "@eigenlayer-middleware/src/interfaces/IStakeRegistry.sol";
-import {IBLSApkRegistry} from "@eigenlayer-middleware/src/interfaces/IBLSApkRegistry.sol";
-import {ISocketRegistry} from "@eigenlayer-middleware/src/interfaces/ISocketRegistry.sol";
-import {IAllocationManager} from "@eigenlayer/contracts/interfaces/IAllocationManager.sol";
-import {OperatorSet} from "@eigenlayer/contracts/libraries/OperatorSetLib.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {BN254} from "@eigenlayer-middleware/src/libraries/BN254.sol";
 
-import {WavsServiceManager} from "src/eigenlayer/bls/WavsServiceManager.sol";
+import {WavsListOperatorsLib} from "script/eigenlayer/bls/utils/WavsListOperatorsLib.sol";
 
 /**
  * @title WavsListOperators
@@ -22,231 +12,22 @@ import {WavsServiceManager} from "src/eigenlayer/bls/WavsServiceManager.sol";
  * @dev This script is used to list the operators for the WAVS service manager.
  */
 contract WavsListOperators is Script {
-    /**
-     * @notice The operator info struct.
-     * @param stakeRegistry The stake registry address.
-     * @param totalWeight The total weight of the operators.
-     * @param minimumStake The minimum stake of the operators.
-     * @param operators The operators.
-     * @param weights The weights of the operators.
-     * @param strategies The strategies of the operators.
-     */
-    struct OperatorInfo {
-        address stakeRegistry;
-        uint96 totalWeight;
-        uint96 minimumStake;
-        address[] operators;
-        bytes32[] operatorIds;
-        BN254.G1Point[] pubkeys;
-        BN254.G2Point[] pubkeyG2s;
-        string[] sockets;
-        uint96[] stakes;
-        IStakeRegistry.StrategyParams[] strategies;
-    }
-
     /// @notice The environment variable for the WAVS service manager address.
     string public constant ENV_SERVICE_MANAGER = "WAVS_SERVICE_MANAGER_ADDRESS";
 
-    WavsServiceManager private serviceManager;
-    uint256 private _quorumNumerator;
-    uint256 private _quorumDenominator;
+    address private _serviceManager;
 
     /// @notice The setup function for the script.
     function setUp() public virtual {
-        serviceManager = WavsServiceManager(vm.envAddress(ENV_SERVICE_MANAGER));
+        _serviceManager = vm.envAddress(ENV_SERVICE_MANAGER);
     }
 
     /// @notice The run function for the script.
     function run() external {
         vm.startBroadcast();
-        OperatorInfo memory opInfo =
-            _listOperators(serviceManager.getRegistryCoordinator(), uint8(0));
-        _quorumNumerator = serviceManager.quorumNumerator();
-        _quorumDenominator = serviceManager.quorumDenominator();
-        _writeOperatorListJson(opInfo);
+        WavsListOperatorsLib.ConfigData memory configData =
+            WavsListOperatorsLib.getConfigData(_serviceManager, uint8(0));
+        WavsListOperatorsLib.writeOperatorListJson(configData);
         vm.stopBroadcast();
-
-        console.log("=== List Operators ===");
-        console.log("Service Manager Address:", address(serviceManager));
-        console.log("Stake Registry Address:", serviceManager.getStakeRegistry());
-        console.log("Strategies:");
-        for (uint256 i = 0; i < opInfo.strategies.length; ++i) {
-            console.log(
-                string.concat(
-                    "Strategy ",
-                    Strings.toString(i),
-                    ": ",
-                    Strings.toHexString(uint160(address(opInfo.strategies[i].strategy)), 20),
-                    " (",
-                    Strings.toString(opInfo.strategies[i].multiplier),
-                    ")"
-                )
-            );
-        }
-
-        console.log(" "); // Blank line for separation
-        console.log("=== Quorum Information ===");
-        console.log(string.concat("Total Weight: ", Strings.toString(uint256(opInfo.totalWeight))));
-        console.log(
-            string.concat("Minimum Stake: ", Strings.toString(uint256(opInfo.minimumStake)))
-        );
-
-        console.log(" "); // Blank line for separation
-        console.log("=== Registered Operators ===");
-        for (uint256 i = 0; i < opInfo.operators.length; ++i) {
-            string memory op = string.concat(
-                "Operator ",
-                Strings.toString(i + 1),
-                ": ",
-                Strings.toHexString(uint160(opInfo.operators[i]), 20)
-            );
-            string memory stake = string.concat("= ", Strings.toString(uint256(opInfo.stakes[i])));
-            console.log(op, stake);
-        }
-
-        console.log(" "); // Blank line for separation
-        console.log("=== Service Manager Quorum Information ===");
-        console.log(string.concat("Quorum Numerator: ", Strings.toString(_quorumNumerator)));
-        console.log(string.concat("Quorum Denominator: ", Strings.toString(_quorumDenominator)));
-    }
-
-    /**
-     * @notice The list operators function.
-     * @param _slashingRegistryCoordinator The slashing registry coordinator address.
-     * @param _quorumNumber The quorum number.
-     * @return The operator info.
-     */
-    function _listOperators(
-        address _slashingRegistryCoordinator,
-        uint8 _quorumNumber
-    ) private view returns (OperatorInfo memory) {
-        ISlashingRegistryCoordinator slashingRegistryCoordinator =
-            ISlashingRegistryCoordinator(_slashingRegistryCoordinator);
-        IStakeRegistry stakeRegistry = slashingRegistryCoordinator.stakeRegistry();
-        ISocketRegistry socketRegistry = slashingRegistryCoordinator.socketRegistry();
-        IBLSApkRegistry blsApkRegistry = slashingRegistryCoordinator.blsApkRegistry();
-
-        uint96 totalWeight = stakeRegistry.getCurrentTotalStake(_quorumNumber);
-
-        IAllocationManager allocationManager =
-            IAllocationManager(serviceManager.getAllocationManager());
-        OperatorSet memory opSetQuery =
-            OperatorSet({avs: address(serviceManager), id: _quorumNumber});
-        address[] memory operators = allocationManager.getMembers(opSetQuery);
-        uint256 operatorCount = operators.length;
-
-        bytes32[] memory operatorIds = new bytes32[](operatorCount);
-        BN254.G1Point[] memory pubkeys = new BN254.G1Point[](operatorCount);
-        BN254.G2Point[] memory pubkeyG2s = new BN254.G2Point[](operatorCount);
-        string[] memory sockets = new string[](operatorCount);
-        uint96[] memory stakes = new uint96[](operatorCount);
-
-        for (uint256 i = 0; i < operatorCount; ++i) {
-            (BN254.G1Point memory pubkey, bytes32 operatorId) =
-                blsApkRegistry.getRegisteredPubkey(operators[i]);
-            operatorIds[i] = operatorId;
-            pubkeys[i] = pubkey;
-            pubkeyG2s[i] = blsApkRegistry.getOperatorPubkeyG2(operators[i]);
-            sockets[i] = socketRegistry.getOperatorSocket(operatorIds[i]);
-            stakes[i] = stakeRegistry.getCurrentStake(operatorIds[i], _quorumNumber);
-        }
-
-        uint256 strategyParamsLength = stakeRegistry.strategyParamsLength(_quorumNumber);
-        IStakeRegistry.StrategyParams[] memory strategies =
-            new IStakeRegistry.StrategyParams[](strategyParamsLength);
-        for (uint256 i = 0; i < strategyParamsLength; ++i) {
-            strategies[i] = stakeRegistry.strategyParamsByIndex(_quorumNumber, i);
-        }
-
-        return OperatorInfo({
-            stakeRegistry: address(stakeRegistry),
-            totalWeight: totalWeight,
-            minimumStake: stakeRegistry.minimumStakeForQuorum(_quorumNumber),
-            operators: operators,
-            operatorIds: operatorIds,
-            pubkeys: pubkeys,
-            pubkeyG2s: pubkeyG2s,
-            sockets: sockets,
-            stakes: stakes,
-            strategies: strategies
-        });
-    }
-
-    /**
-     * @notice The write operator list JSON function.
-     * @param opInfo The operator info.
-     */
-    function _writeOperatorListJson(
-        OperatorInfo memory opInfo
-    ) internal {
-        if (!vm.exists("deployments/wavs-bls")) {
-            vm.createDir("deployments/wavs-bls", true);
-        }
-
-        string memory json = "{\"stakeRegistry\":\"";
-        json = string.concat(json, Strings.toHexString(uint160(opInfo.stakeRegistry), 20));
-        json = string.concat(json, "\",\"totalWeight\":\"");
-        json = string.concat(json, Strings.toString(opInfo.totalWeight));
-        json = string.concat(json, "\",\"minimumStake\":\"");
-        json = string.concat(json, Strings.toString(opInfo.minimumStake));
-        json = string.concat(json, "\",\"strategies\":[");
-
-        for (uint256 i = 0; i < opInfo.strategies.length; ++i) {
-            if (i > 0) {
-                json = string.concat(json, ",");
-            }
-            json = string.concat(json, "{\"strategy\":\"");
-            json = string.concat(
-                json, Strings.toHexString(uint160(address(opInfo.strategies[i].strategy)), 20)
-            );
-            json = string.concat(json, "\",\"multiplier\":\"");
-            json = string.concat(json, Strings.toString(opInfo.strategies[i].multiplier));
-            json = string.concat(json, "\"}");
-        }
-
-        json = string.concat(json, "],\"operators\":[");
-
-        for (uint256 i = 0; i < opInfo.operators.length; ++i) {
-            if (i > 0) {
-                json = string.concat(json, ",");
-            }
-            json = string.concat(json, "{\"operator\":\"");
-            json = string.concat(json, Strings.toHexString(uint160(opInfo.operators[i]), 20));
-            json = string.concat(json, "\",\"operatorId\":\"");
-            json = string.concat(json, Strings.toHexString(uint256(opInfo.operatorIds[i]), 32));
-            json = string.concat(json, "\",\"pubkey\":{");
-            json = string.concat(json, "\"x\":\"");
-            json = string.concat(json, Strings.toHexString(uint256(opInfo.pubkeys[i].X), 32));
-            json = string.concat(json, "\",\"y\":\"");
-            json = string.concat(json, Strings.toHexString(uint256(opInfo.pubkeys[i].Y), 32));
-            json = string.concat(json, "\"},");
-            json = string.concat(json, "\"pubkeyG2\":{");
-            json = string.concat(json, "\"x\":[\"");
-            json = string.concat(json, Strings.toHexString(uint256(opInfo.pubkeyG2s[i].X[0]), 32));
-            json = string.concat(
-                json, "\",\"", Strings.toHexString(uint256(opInfo.pubkeyG2s[i].X[1]), 32)
-            );
-            json = string.concat(json, "\"],");
-            json = string.concat(json, "\"y\":[\"");
-            json = string.concat(json, Strings.toHexString(uint256(opInfo.pubkeyG2s[i].Y[0]), 32));
-            json = string.concat(
-                json, "\",\"", Strings.toHexString(uint256(opInfo.pubkeyG2s[i].Y[1]), 32)
-            );
-            json = string.concat(json, "\"]},");
-            json = string.concat(json, "\"socket\":\"");
-            json = string.concat(json, opInfo.sockets[i]);
-            json = string.concat(json, "\",\"stake\":\"");
-            json = string.concat(json, Strings.toString(opInfo.stakes[i]));
-            json = string.concat(json, "\"}");
-        }
-
-        json = string.concat(json, "],\"quorumNumerator\":\"");
-        json = string.concat(json, Strings.toString(_quorumNumerator));
-        json = string.concat(json, "\",\"quorumDenominator\":\"");
-        json = string.concat(json, Strings.toString(_quorumDenominator));
-        json = string.concat(json, "\"");
-        json = string.concat(json, "}");
-
-        vm.writeFile("deployments/wavs-bls/list_operators.json", json);
     }
 }
---
+++ contracts/script/eigenlayer/bls/utils/WavsListOperatorsLib.sol
@@ -0,0 +1,300 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Vm} from "forge-std/Vm.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {BN254} from "@eigenlayer-middleware/src/libraries/BN254.sol";
+import {ISlashingRegistryCoordinator} from
+    "@eigenlayer-middleware/src/interfaces/ISlashingRegistryCoordinator.sol";
+import {
+    IStakeRegistry,
+    IStakeRegistryTypes
+} from "@eigenlayer-middleware/src/interfaces/IStakeRegistry.sol";
+import {IBLSApkRegistry} from "@eigenlayer-middleware/src/interfaces/IBLSApkRegistry.sol";
+import {ISocketRegistry} from "@eigenlayer-middleware/src/interfaces/ISocketRegistry.sol";
+import {IAllocationManager} from "@eigenlayer/contracts/interfaces/IAllocationManager.sol";
+import {OperatorSet} from "@eigenlayer/contracts/libraries/OperatorSetLib.sol";
+import {IStrategy} from "@eigenlayer/contracts/interfaces/IStrategy.sol";
+import {WavsServiceManager} from "src/eigenlayer/bls/WavsServiceManager.sol";
+
+/**
+ * @title WavsListOperatorsLib
+ * @author Lay3rLabs
+ * @notice This library contains the functions for listing the operators for the WAVS service manager.
+ * @dev This library is used to list the operators for the WAVS service manager.
+ */
+library WavsListOperatorsLib {
+    /* solhint-disable gas-struct-packing */
+    /**
+     * @notice The operator data struct.
+     * @param operator The operator address.
+     * @param operatorId The operator ID.
+     * @param pubkey The public key.
+     * @param pubkeyG2 The G2 public key.
+     * @param socket The socket information.
+     * @param stake The stake amount.
+     */
+    struct OperatorData {
+        address operator;
+        bytes32 operatorId;
+        BN254.G1Point pubkey;
+        BN254.G2Point pubkeyG2;
+        string socket;
+        uint96 stake;
+    }
+    /* solhint-enable gas-struct-packing */
+
+    /**
+     * @notice The config data struct.
+     * @param totalWeight The total weight.
+     * @param minimumStake The minimum stake.
+     * @param strategies The strategy parameters.
+     * @param operators The operator data.
+     * @param quorumNumerator The quorum numerator.
+     * @param quorumDenominator The quorum denominator.
+     */
+    struct ConfigData {
+        uint96 totalWeight;
+        uint96 minimumStake;
+        IStakeRegistry.StrategyParams[] strategies;
+        OperatorData[] operators;
+        uint256 quorumNumerator;
+        uint256 quorumDenominator;
+    }
+
+    Vm internal constant VM = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    /// @notice The error for the config file not found.
+    error WavsListOperatorsLib__ConfigFileNotFound();
+
+    /**
+     * @notice The get config data function.
+     * @param _serviceManager The service manager address.
+     * @param _quorumNumber The quorum number.
+     * @return configData The config data.
+     */
+    function getConfigData(
+        address _serviceManager,
+        uint8 _quorumNumber
+    ) internal view returns (ConfigData memory configData) {
+        WavsServiceManager serviceManager = WavsServiceManager(_serviceManager);
+        configData.quorumNumerator = serviceManager.quorumNumerator();
+        configData.quorumDenominator = serviceManager.quorumDenominator();
+
+        ISlashingRegistryCoordinator slashingRegistryCoordinator =
+            ISlashingRegistryCoordinator(serviceManager.getRegistryCoordinator());
+        IStakeRegistry stakeRegistry = slashingRegistryCoordinator.stakeRegistry();
+        ISocketRegistry socketRegistry = slashingRegistryCoordinator.socketRegistry();
+        IBLSApkRegistry blsApkRegistry = slashingRegistryCoordinator.blsApkRegistry();
+
+        configData.totalWeight = stakeRegistry.getCurrentTotalStake(_quorumNumber);
+        configData.minimumStake = stakeRegistry.minimumStakeForQuorum(_quorumNumber);
+
+        IAllocationManager allocationManager =
+            IAllocationManager(serviceManager.getAllocationManager());
+        OperatorSet memory opSetQuery =
+            OperatorSet({avs: address(serviceManager), id: _quorumNumber});
+        address[] memory operators = allocationManager.getMembers(opSetQuery);
+        uint256 operatorCount = operators.length;
+
+        OperatorData[] memory operatorData = new OperatorData[](operatorCount);
+
+        for (uint256 i = 0; i < operatorCount; ++i) {
+            (BN254.G1Point memory pubkey, bytes32 operatorId) =
+                blsApkRegistry.getRegisteredPubkey(operators[i]);
+            operatorData[i] = OperatorData({
+                operator: operators[i],
+                operatorId: operatorId,
+                pubkey: pubkey,
+                pubkeyG2: blsApkRegistry.getOperatorPubkeyG2(operators[i]),
+                socket: socketRegistry.getOperatorSocket(operatorId),
+                stake: stakeRegistry.getCurrentStake(operatorId, _quorumNumber)
+            });
+        }
+
+        uint256 strategyParamsLength = stakeRegistry.strategyParamsLength(_quorumNumber);
+        IStakeRegistry.StrategyParams[] memory strategies =
+            new IStakeRegistry.StrategyParams[](strategyParamsLength);
+        for (uint256 i = 0; i < strategyParamsLength; ++i) {
+            strategies[i] = stakeRegistry.strategyParamsByIndex(_quorumNumber, i);
+        }
+
+        configData.strategies = strategies;
+        configData.operators = operatorData;
+
+        return configData;
+    }
+
+    /**
+     * @notice The read config function.
+     * @param fileName The file name.
+     * @return configData The config data.
+     */
+    function readConfig(
+        string memory fileName
+    ) internal returns (WavsListOperatorsLib.ConfigData memory) {
+        if (!VM.exists(fileName)) {
+            revert WavsListOperatorsLib__ConfigFileNotFound();
+        }
+
+        // load the complete config
+        string memory json = VM.readFile(fileName);
+
+        // Parse basic config data
+        uint96 minimumStake = abi.decode(VM.parseJson(json, ".minimumStake"), (uint96));
+        uint96 totalWeight = abi.decode(VM.parseJson(json, ".totalWeight"), (uint96));
+        uint256 quorumNumerator = abi.decode(VM.parseJson(json, ".quorumNumerator"), (uint256));
+        uint256 quorumDenominator = abi.decode(VM.parseJson(json, ".quorumDenominator"), (uint256));
+
+        // Parse the strategies array from the JSON
+        address[] memory strategies =
+            abi.decode(VM.parseJson(json, ".strategies[*].strategy"), (address[]));
+        uint256 strategyCount = strategies.length;
+        uint96[] memory multipliers =
+            abi.decode(VM.parseJson(json, ".strategies[*].multiplier"), (uint96[]));
+
+        // Convert to strategy params
+        IStakeRegistryTypes.StrategyParams[] memory strategyParams =
+            new IStakeRegistryTypes.StrategyParams[](strategyCount);
+        for (uint256 i; i < strategyCount; ++i) {
+            strategyParams[i] = IStakeRegistryTypes.StrategyParams({
+                strategy: IStrategy(strategies[i]),
+                multiplier: multipliers[i]
+            });
+        }
+
+        // Parse operators data
+        address[] memory operatorAddresses =
+            abi.decode(VM.parseJson(json, ".operators[*].operator"), (address[]));
+        bytes32[] memory operatorIds =
+            abi.decode(VM.parseJson(json, ".operators[*].operatorId"), (bytes32[]));
+        uint256[] memory pubkeyXs =
+            abi.decode(VM.parseJson(json, ".operators[*].pubkey.x"), (uint256[]));
+        uint256[] memory pubkeyYs =
+            abi.decode(VM.parseJson(json, ".operators[*].pubkey.y"), (uint256[]));
+        string[] memory sockets = abi.decode(VM.parseJson(json, ".operators[*].socket"), (string[]));
+        uint96[] memory stakes = abi.decode(VM.parseJson(json, ".operators[*].stake"), (uint96[]));
+
+        // Parse G2 public keys (arrays of 2 uint256s)
+        uint256[2][] memory pubkeyG2Xs =
+            abi.decode(VM.parseJson(json, ".operators[*].pubkeyG2.x"), (uint256[2][]));
+        uint256[2][] memory pubkeyG2Ys =
+            abi.decode(VM.parseJson(json, ".operators[*].pubkeyG2.y"), (uint256[2][]));
+
+        uint256 operatorCount = operatorAddresses.length;
+
+        // Convert to operator data
+        OperatorData[] memory operators = new OperatorData[](operatorCount);
+        for (uint256 i; i < operatorCount; ++i) {
+            operators[i] = OperatorData({
+                operator: operatorAddresses[i],
+                operatorId: operatorIds[i],
+                pubkey: BN254.G1Point({X: pubkeyXs[i], Y: pubkeyYs[i]}),
+                pubkeyG2: BN254.G2Point({X: pubkeyG2Xs[i], Y: pubkeyG2Ys[i]}),
+                socket: sockets[i],
+                stake: stakes[i]
+            });
+        }
+
+        return ConfigData({
+            totalWeight: totalWeight,
+            minimumStake: minimumStake,
+            strategies: strategyParams,
+            operators: operators,
+            quorumNumerator: quorumNumerator,
+            quorumDenominator: quorumDenominator
+        });
+    }
+
+    /**
+     * @notice The write operator list JSON function.
+     * @param configData The config data.
+     */
+    function writeOperatorListJson(
+        ConfigData memory configData
+    ) internal {
+        if (!VM.exists("deployments/wavs-bls")) {
+            VM.createDir("deployments/wavs-bls", true);
+        }
+
+        string memory json = "{\"totalWeight\":\"";
+        json = string.concat(json, Strings.toString(configData.totalWeight));
+        json = string.concat(json, "\",\"minimumStake\":\"");
+        json = string.concat(json, Strings.toString(configData.minimumStake));
+        json = string.concat(json, "\",\"strategies\":[");
+
+        for (uint256 i = 0; i < configData.strategies.length; ++i) {
+            if (i > 0) {
+                json = string.concat(json, ",");
+            }
+            json = string.concat(json, "{\"strategy\":\"");
+            json = string.concat(
+                json, Strings.toHexString(uint160(address(configData.strategies[i].strategy)), 20)
+            );
+            json = string.concat(json, "\",\"multiplier\":\"");
+            json = string.concat(json, Strings.toString(configData.strategies[i].multiplier));
+            json = string.concat(json, "\"}");
+        }
+
+        json = string.concat(json, "],\"operators\":[");
+
+        for (uint256 i = 0; i < configData.operators.length; ++i) {
+            if (i > 0) {
+                json = string.concat(json, ",");
+            }
+            json = string.concat(json, "{\"operator\":\"");
+            json = string.concat(
+                json, Strings.toHexString(uint160(configData.operators[i].operator), 20)
+            );
+            json = string.concat(json, "\",\"operatorId\":\"");
+            json = string.concat(
+                json, Strings.toHexString(uint256(configData.operators[i].operatorId), 32)
+            );
+            json = string.concat(json, "\",\"pubkey\":{");
+            json = string.concat(json, "\"x\":\"");
+            json = string.concat(
+                json, Strings.toHexString(uint256(configData.operators[i].pubkey.X), 32)
+            );
+            json = string.concat(json, "\",\"y\":\"");
+            json = string.concat(
+                json, Strings.toHexString(uint256(configData.operators[i].pubkey.Y), 32)
+            );
+            json = string.concat(json, "\"},");
+            json = string.concat(json, "\"pubkeyG2\":{");
+            json = string.concat(json, "\"x\":[\"");
+            json = string.concat(
+                json, Strings.toHexString(uint256(configData.operators[i].pubkeyG2.X[0]), 32)
+            );
+            json = string.concat(
+                json,
+                "\",\"",
+                Strings.toHexString(uint256(configData.operators[i].pubkeyG2.X[1]), 32)
+            );
+            json = string.concat(json, "\"],");
+            json = string.concat(json, "\"y\":[\"");
+            json = string.concat(
+                json, Strings.toHexString(uint256(configData.operators[i].pubkeyG2.Y[0]), 32)
+            );
+            json = string.concat(
+                json,
+                "\",\"",
+                Strings.toHexString(uint256(configData.operators[i].pubkeyG2.Y[1]), 32)
+            );
+            json = string.concat(json, "\"]},");
+            json = string.concat(json, "\"socket\":\"");
+            json = string.concat(json, configData.operators[i].socket);
+            json = string.concat(json, "\",\"stake\":\"");
+            json = string.concat(json, Strings.toString(configData.operators[i].stake));
+            json = string.concat(json, "\"}");
+        }
+
+        json = string.concat(json, "],\"quorumNumerator\":\"");
+        json = string.concat(json, Strings.toString(configData.quorumNumerator));
+        json = string.concat(json, "\",\"quorumDenominator\":\"");
+        json = string.concat(json, Strings.toString(configData.quorumDenominator));
+        json = string.concat(json, "\"");
+        json = string.concat(json, "}");
+
+        VM.writeFile("deployments/wavs-bls/list_operators.json", json);
+    }
+}
